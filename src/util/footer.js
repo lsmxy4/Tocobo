fd3128920c0fd0ff037182c76df7f8e0e15f6c77
unknown <lsmxy4@gmail.com>
2026-03-13 19:29:03 +0900

    finish

--- src/util/footer.js
+++ src/util/footer.js
@@ -5,8 +5,8 @@ import { RiKakaoTalkFill } from "react-icons/ri";
 
 // 로고
 export const logoData = {
-  src: img("logo.svg"),
-  alt: "TOCOBO 로고",
+  src: img("Logos.png"),
+  alt: "Atomy 로고",
   href: "/",
 };
 

ee6b1b217b8bf7d102deac055df2af00d19958b0
unknown <lsmxy4@gmail.com>
2026-03-18 21:26:29 +0900

    final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Nav from './Nav'
 import Util from './Util'
-import './styles/Header.scss'
+import './style/Header.scss'
 import { headerData } from '../util/header'
 import Mnav from './Mnav'
 import title from '../assets/Clip path group.png'
